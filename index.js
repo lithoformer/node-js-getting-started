9f19e1cf878e5e11a532547f8dd6acfbd973df7e
lithoformer <zhiguang53846@hotmail.com>
2024-07-09 19:36:09 -0400

    added dependencies and discogs code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const discogs = require('disconnect').Client;
 
 const PORT = process.env.PORT || 5001
 
@@ -8,4 +9,10 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+const records = discogs().setConfig({ outputFormat: 'html' });
+const db = records.user().collection();
+db.getReleases('b1furc4t0r', 0, { page: 0, per_page: 100 }, function (err, data) {
+  console.log(data.releases[0].basic_information.artists[0]);
+});
